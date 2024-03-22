7cd1ffbd90eebf6258417ac24efc16d043ecd59f
Victor Manuel <115516580+Programador346@users.noreply.github.com>
2024-03-22 17:47:34 -0500

    Mejoras
    Integracion de un mejor diseño y icono con diseño propio

--- src/screens/views/PasswordForm.jsx
+++ src/screens/views/PasswordForm.jsx
@@ -49,19 +49,19 @@ const PasswordForm = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen" style={{ backgroundColor: 'navy', margin: 0, padding: 0, border: 'none' }}>
-      <div className="w-full max-w-md bg-gray-200 p-6 rounded-md shadow-md">
-        <h2 className="text-2xl font-bold mb-5 text-center">Cambiar Contraseña</h2>
+    <div className="flex items-center justify-center min-h-screen" style={{ backgroundImage: 'url("https://images.pexels.com/photos/188035/pexels-photo-188035.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',backgroundSize: 'cover',backgroundPosition: 'center', margin: 0, padding: 0}}>
+      <div className="w-full max-w-md bg-gradient-to-br from-gray-400 to-gray-60 p-6 rounded-md shadow-md">
+        <h2 className="text-2xl font-serif mb-5 text-center">Cambiar Contraseña</h2>
         <form>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            <label className="block text-gray-700 text-sm font-serif mb-2" htmlFor="password">
               Nueva Contraseña
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
               type="password"
-              placeholder=""
+              placeholder="**********"
               value={password}
               onChange={handleInputChange}
               style={{ transition: 'all .3s ease' }}
@@ -71,7 +71,7 @@ const PasswordForm = () => {
           </div>
           <div className="flex items-center justify-center">
             <button
-              className="bg-purple-600 hover:bg-purple-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transform transition duration-500 ease-in-out"
+              className="bg-gradient-to-br from-orange-400 to-orange-600 hover:from-orange-300 hover:to-orange-500 text-slate-900 bg-orange-400 bg-opacity-50 text-white font-serif py-2 px-4 rounded focus:outline-none focus:shadow-outline transform transition duration-500 ease-in-out"
               type="button"
               onClick={handleSendPassword}
               onMouseOver={(e) => { e.target.style.boxShadow = '0 0 5px 2px #9f7aea'; e.target.style.transform = 'scale(1.05)' }}
