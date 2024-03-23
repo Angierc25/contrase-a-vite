03f387ea7edb904a97aa6caa40bb3386bf4ef4ab
Victor Manuel <115516580+Programador346@users.noreply.github.com>
2024-03-23 18:55:31 -0500

    Añadir Mostrar Contraseña

--- src/screens/views/PasswordForm.jsx
+++ src/screens/views/PasswordForm.jsx
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const PasswordForm = () => {
   const [password, setPassword] = useState('');
   const [token, setToken] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
-  // Función para obtener el token de la URL
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
     const urlToken = searchParams.get('token');
@@ -48,26 +50,35 @@ const PasswordForm = () => {
     setPassword(value);
   };
 
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen" style={{ backgroundImage: 'url("https://images.pexels.com/photos/188035/pexels-photo-188035.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")',backgroundSize: 'cover',backgroundPosition: 'center', margin: 0, padding: 0}}>
       <div className="w-full max-w-md bg-gradient-to-br from-gray-400 to-gray-60 p-6 rounded-md shadow-md">
         <h2 className="text-2xl font-serif mb-5 text-center">Cambiar Contraseña</h2>
         <form>
-          <div className="mb-4">
+          <div className="mb-4 relative">
             <label className="block text-gray-700 text-sm font-serif mb-2" htmlFor="password">
               Nueva Contraseña
             </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              type="password"
-              placeholder="**********"
-              value={password}
-              onChange={handleInputChange}
-              style={{ transition: 'all .3s ease' }}
-              onFocus={(e) => { e.target.style.boxShadow = '0 0 5px 2px #3b82f6'; e.target.style.transform = 'scale(1.05)' }}
-              onBlur={(e) => { e.target.style.boxShadow = 'none'; e.target.style.transform = 'scale(1.0)' }}
-            />
+            <div className="flex items-center">
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type={showPassword ? 'text' : 'password'}
+                placeholder=""
+                value={password}
+                onChange={handleInputChange}
+                style={{ transition: 'all .3s ease' }}
+                onFocus={(e) => { e.target.style.boxShadow = '0 0 5px 2px #3b82f6'; e.target.style.transform = 'scale(1.05)' }}
+                onBlur={(e) => { e.target.style.boxShadow = 'none'; e.target.style.transform = 'scale(1.0)' }}
+              />
+              <span className="ml-2 cursor-pointer" onClick={toggleShowPassword}>
+                <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className="text-slate-950" />
+              </span>
+            </div>
           </div>
           <div className="flex items-center justify-center">
             <button
