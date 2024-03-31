f10037f025ff7e2838f1d4f590ed556039c7ff95
Victor Manuel <115516580+Programador346@users.noreply.github.com>
2024-03-31 15:40:32 -0500

    Mejora

--- src/screens/views/PasswordForm.jsx
+++ src/screens/views/PasswordForm.jsx
@@ -59,9 +59,9 @@ const PasswordForm = () => {
       <img
         src="https://images.pexels.com/photos/188035/pexels-photo-188035.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
         alt="Imagen de fondo"
-        className="absolute inset-0 w-full h-full object-cover z-0"
+        className="absolute -inset-x-0  -inset-y-px w-full h-full object-cover z-0"
       />
-      <div className="relative z-full w-full max-w-md bg-gradient-to-br from-gray-400 to-gray-60 p-6 rounded-md shadow-md">
+      <div className="relative z-full w-full max-w-md bg-gradient-to-br from-gray-400 to-gray-60 p-6 rounded-md shadow-md ">
         <h2 className="text-2xl font-serif mb-5 text-center">Cambiar Contraseña</h2>
         <form >
           <div className="mb-4 relative">
@@ -97,11 +97,11 @@ const PasswordForm = () => {
             </button>
           </div>
         </form>
-        <p className="text-center text-gray-500 text-xs mt-3">
+        <p className="text-center text-gray-700 text-xs ">
           &copy;2024 Mi Empresa. Todos los derechos reservados.
         </p>
       </div>
-    </div>
+      </div>
   );
 };
 
