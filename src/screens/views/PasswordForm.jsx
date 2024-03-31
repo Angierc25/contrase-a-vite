7c54545a369378ddc5343d5af263326b655a15d2
Victor Manuel <115516580+Programador346@users.noreply.github.com>
2024-03-31 16:40:07 -0500

    Diseño Finalizado

--- src/screens/views/PasswordForm.jsx
+++ src/screens/views/PasswordForm.jsx
@@ -61,49 +61,51 @@ const PasswordForm = () => {
         alt="Imagen de fondo"
         className="absolute -inset-x-0  -inset-y-px w-full h-full object-cover z-0"
       />
-      <div className="justify-center items-center p-48 left-80">
-      <div className="relative z-full w-full max-w-md bg-gradient-to-br from-gray-400 to-gray-60 p-6 rounded-md shadow-md ">
-        <h2 className="text-2xl font-serif mb-5 text-center">Cambiar Contraseña</h2>
-        <form >
-          <div className="mb-4 relative">
-            <label className="block text-gray-700 text-sm font-serif mb-2" htmlFor="password">
-              Nueva Contraseña
-            </label>
-            <div className="flex items-center">
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type={showPassword ? 'text' : 'password'}
-                placeholder=""
-                value={password}
-                onChange={handleInputChange}
-                style={{ transition: 'all .3s ease' }}
-                onFocus={(e) => { e.target.style.boxShadow = '0 0 5px 2px #3b82f6'; e.target.style.transform = 'scale(1.05)' }}
-                onBlur={(e) => { e.target.style.boxShadow = 'none'; e.target.style.transform = 'scale(1.0)' }}
-              />
-              <span className="ml-2 cursor-pointer" onClick={toggleShowPassword}>
-                <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className="text-slate-950" />
-              </span>
-            </div>
+      <div className='mx-auto'>
+        <div className="justify-center items-center p-48" >
+          <div className="relative z-full w-full max-w-md bg-gradient-to-br from-gray-400 to-gray-60 p-6 rounded-md shadow-md ">
+            <h2 className="text-2xl font-serif mb-5 text-center">Cambiar Contraseña</h2>
+            <form >
+              <div className="mb-4 relative">
+                <label className="block text-gray-700 text-sm font-serif mb-2" htmlFor="password">
+                  Nueva Contraseña
+                </label>
+                <div className="flex items-center">
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="password"
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder=""
+                    value={password}
+                    onChange={handleInputChange}
+                    style={{ transition: 'all .3s ease' }}
+                    onFocus={(e) => { e.target.style.boxShadow = '0 0 5px 2px #3b82f6'; e.target.style.transform = 'scale(1.05)' }}
+                    onBlur={(e) => { e.target.style.boxShadow = 'none'; e.target.style.transform = 'scale(1.0)' }}
+                  />
+                  <span className="ml-2 cursor-pointer" onClick={toggleShowPassword}>
+                    <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} className="text-slate-950" />
+                  </span>
+                </div>
+              </div>
+              <div className="flex items-center justify-center">
+                <button
+                  className="bg-gradient-to-br from-orange-400 to-orange-600 hover:from-orange-300 hover:to-orange-500 text-slate-900 bg-orange-400 bg-opacity-50 text-white font-serif py-2 px-4 rounded focus:outline-none focus:shadow-outline transform transition duration-500 ease-in-out"
+                  type="button"
+                  onClick={handleSendPassword}
+                  onMouseOver={(e) => { e.target.style.boxShadow = '0 0 5px 2px #9f7aea'; e.target.style.transform = 'scale(1.05)' }}
+                  onMouseOut={(e) => { e.target.style.boxShadow = 'none'; e.target.style.transform = 'scale(1.0)' }}
+                >
+                  Cambiar Contraseña
+                </button>
+              </div>
+            </form>
+            <p className="text-center text-gray-700 text-xs ">
+              &copy;2024 Mi Empresa. Todos los derechos reservados.
+            </p>
           </div>
-          <div className="flex items-center justify-center">
-            <button
-              className="bg-gradient-to-br from-orange-400 to-orange-600 hover:from-orange-300 hover:to-orange-500 text-slate-900 bg-orange-400 bg-opacity-50 text-white font-serif py-2 px-4 rounded focus:outline-none focus:shadow-outline transform transition duration-500 ease-in-out"
-              type="button"
-              onClick={handleSendPassword}
-              onMouseOver={(e) => { e.target.style.boxShadow = '0 0 5px 2px #9f7aea'; e.target.style.transform = 'scale(1.05)' }}
-              onMouseOut={(e) => { e.target.style.boxShadow = 'none'; e.target.style.transform = 'scale(1.0)' }}
-            >
-              Cambiar Contraseña
-            </button>
-          </div>
-        </form>
-        <p className="text-center text-gray-700 text-xs ">
-          &copy;2024 Mi Empresa. Todos los derechos reservados.
-        </p>
+        </div>
       </div>
-      </div>
-      </div>
+    </div>
   );
 };
 
